3e136b76aac03b0d6c820043201d97ac24d85550
Chris <chris.is.phillips@gmail.com>
2017-04-01 13:36:47 +0300

    now shows players in room

--- client/js/components/homepage/Room.js
+++ client/js/components/homepage/Room.js
@@ -11,11 +11,19 @@ export default class Room extends Component {
 		this.props.history.push('/Folring');
 	}
 
+	spectateRoom() {
+
+	}
+
 	render() {
 		console.log('RECEIVING ROOM', this.props.room);
+		const room = this.props.room;
 		return (
 			<div onClick={this.joinRoom.bind(this)} className="room">
-				{this.props.room.id}
+				<p>{room.id}</p>
+				<p>Player 1: {room.players[0] ? room.players[0].username : <button onClick={this.joinRoom.bind(this, 0)}>Join</button>}</p>
+				<p>Player 2: {room.players[1] ? room.players[1].username : <button onClick={this.joinRoom.bind(this, 1)}>Join</button>}</p>
+				<p>Spectators: {this.props.room.spectators.length} <button onClick={this.spectateRoom.bind(this)}>Spectate</button></p>
 			</div>
 		);
 	}
