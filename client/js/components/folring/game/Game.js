66d1a9522faa8878b819710d1c9ee9b09b8ea41a
miikakuisma <miika@funacademy.fi>
2017-04-07 20:52:43 +0300

    Now should be good for mobile!

--- client/js/components/folring/game/Game.js
+++ client/js/components/folring/game/Game.js
@@ -24,6 +24,15 @@ export default class Game extends React.Component {
     }
   }
 
+  componentDidMount() {
+    // Set correct height and vertical position for the game board
+    const board = document.querySelector('.folring-holder')
+    board.style.height = board.offsetWidth+"px"
+
+    const messaging = document.querySelector('#messaging')
+    messaging.style.height = "calc(100% - "+parseInt(board.offsetWidth+54)+"px)"
+  }
+
   componentWillReceiveProps(nextProps) {
     if (nextProps.room.game) {
       // Determine which side the player is
